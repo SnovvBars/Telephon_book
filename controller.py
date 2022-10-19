c686effcbd49abfef18a3c36674443a6e092522b
Sergey Volgin <artphoto@mail.ru>
2022-10-19 20:03:30 +0300

    New files - 3

--- controller.py
+++ controller.py
@@ -1,11 +1,15 @@
 import UI as UI
 import r_txt as r_txt
+import search as search
 
 def button_click():
     item = UI.select_main_item()
     if item == 1:
         item = UI.data_input()
         r_txt.write_txt(item)
+    if item == 2:
+        item = search.data_search()
+
     print(item) 
 
 button_click()
--- r_txt.py
+++ r_txt.py
@@ -1,10 +1,28 @@
 from datetime import datetime as dt
 from time import time
 
-# 17:26;data;{'famaly': 'dfaf', 'telephone': '13435', 'own': 2, 'gender': 1, 'st�tus': 2}
+def write_txt(dic):
+    time = dt.now().strftime('%D:%H:%M')
+    for key, value in dic.items():
+        if key == 'own' : dic[key] = finding_own(value)
+        elif key == 'gender' : dic[key] = finding_gender(value)
+        elif key == 'status' : dic[key] = finding_status(value)
+    # print('data: {}, {}\n'
+    #         .format(time, ', '.join('{}: {}'.format(key, val) for key, val in dic.items())))
+    with open('t_book.txt', 'a', encoding = 'UTF_8') as file:
+        file.write('data: {}, {}\n'
+            .format(time, ', '.join('{}: {}'.format(key, val) for key, val in dic.items())))
 
-def write_txt(str):
-    time = dt.now().strftime('%H:%M')
-    with open('t_book.txt', 'a') as file:
-        file.write('{};data;{}\n'
-            .format(time, str))
+def finding_own(owns):
+    if owns == 1: return 'Личный'
+    elif owns == 2: return 'Рабочий'
+    else: return 'Домашний'
+
+def finding_gender(gender):
+    if gender == 1: return 'Мужской'
+    elif gender == 2: return 'Женский'
+    
+def finding_status(status):
+    if status == 1: return 'Женат\Замужем'
+    elif status == 2: return 'Холост'
+    else: return 'В разводе'    
