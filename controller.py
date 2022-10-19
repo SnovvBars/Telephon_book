9bf7e665a1ffa5400e709f450a464d7630bd3438
Sergey Volgin <artphoto@mail.ru>
2022-10-19 20:50:28 +0300

    New files - 4

--- controller.py
+++ controller.py
@@ -4,11 +4,13 @@ import search as search
 
 def button_click():
     item = UI.select_main_item()
+    print(item)
     if item == 1:
         item = UI.data_input()
         r_txt.write_txt(item)
-    if item == 2:
-        item = search.data_search()
+    # elif item == 2:
+    #     item = search.data_search(item)
+    # else : print(item)
 
     print(item) 
 
--- search.py
+++ search.py
@@ -1,1 +1,6 @@
 elem = str(input('Введите первые три буквы фамилии или фамилию целиком: '))
+
+def data_search(str):
+    print (str)
+
+data_search(str)
